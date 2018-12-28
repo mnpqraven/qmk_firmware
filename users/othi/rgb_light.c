e099520a46ab2b14404a0062018258b748387956
Othi <mnpq.raven@gmail.com>
2018-12-28 11:43:37 +0100

    fixed some lines, better rgb calls, now default layer color can be edited in `othi.h`

--- users/othi/rgb_light.c
+++ users/othi/rgb_light.c
@@ -25,7 +25,7 @@ uint32_t layer_state_set_user(uint32_t state) {
             if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_magenta(); rgblight_mode_noeeprom(1); }
             break;
         default: //  for any other layers, or the default layer
-            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_cyan(); rgblight_mode_noeeprom(1); }
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom(OTHI_DEFAULT_R, OTHI_DEFAULT_G, OTHI_DEFAULT_B); rgblight_mode_noeeprom(1); }
             break;
     }
     return state;
@@ -100,24 +100,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; // Process all other keycodes normally
     }
 }
-
-//uint32_t layer_state_set_user(uint32_t state) {
-//    switch (biton32(state)) {
-//    case NM_MODE:
-//        rgblight_setrgb (0x00,  0x66, 0x00);
-//        break;
-//    case VI_MODE:
-//        rgblight_setrgb (0x66,  0x66, 0x00);
-//        break;
-//    case ACCENT:
-//        rgblight_setrgb (0x7A,  0x00, 0xFF);
-//        break;
-//    case ACCENT_CAP:
-//        rgblight_setrgb (0x7A,  0xFF, 0xFF);
-//        break;
-//    default: //  for any other layers, or the default layer
-//        rgblight_setrgb (0x00,  0x00, 0xFF);
-//        break;
-//    }
-//  return state;
-//}
