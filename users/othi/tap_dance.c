f0f3f6f9ed53bbc32ce7cf88347eeeb35c5f8cc9
Othi <mnpq.raven@gmail.com>
2018-12-29 04:19:43 +0100

    added light debouncing, some fix for the dz60 TODO: kbd6x update to keep up with dz60

--- users/othi/tap_dance.c
+++ users/othi/tap_dance.c
@@ -20,6 +20,7 @@ void dance_CTL_NM_reset (qk_tap_dance_state_t *state, void *user_data) {
 void dance_GUI_NM_finished (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
 	register_code (KC_LGUI);
+	_delay_ms(LIGHT_DEBOUNCE);
     rgblight_sethsv_noeeprom_magenta(); rgblight_mode_noeeprom(1);
   } else {
     register_code (KC_LGUI);
@@ -58,7 +59,8 @@ void dance_ALT_NM_reset (qk_tap_dance_state_t *state, void *user_data) {
 void dance_SFT_NM_finished (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     register_code (KC_LSFT);
-    rgblight_sethsv_noeeprom_red(); rgblight_mode_noeeprom(1);
+	_delay_ms(LIGHT_DEBOUNCE);
+    //rgblight_sethsv_noeeprom_orange(); rgblight_mode_noeeprom(1);
   } else {
     register_code (KC_LSFT);
 	layer_on(NM_MODE);
@@ -75,10 +77,33 @@ void dance_SFT_NM_reset (qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
+//NOT FINISHED. DO NOT USE
+void dance_LANG_IN_finished (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_LGUI);
+	tap_code(KC_SPC);
+    //rgblight_sethsv_noeeprom_red(); rgblight_mode_noeeprom(1);
+  } else {
+    register_code (KC_LGUI);
+  }
+}
+
+void dance_LANG_IN_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code (KC_LGUI);
+    //rgblight_sethsv_noeeprom(OTHI_DEFAULT_R, OTHI_DEFAULT_G, OTHI_DEFAULT_B); rgblight_mode_noeeprom(1);
+  } else {
+    unregister_code (KC_LGUI);
+  }
+}
+void dance_LANG_IN_each(qk_tap_dance_state_t *state, void *user_data) {
+	tap_code(KC_SPC);
+}
 qk_tap_dance_action_t tap_dance_actions[] = {
  [CTL_NM] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_CTL_NM_finished, dance_CTL_NM_reset),
  [GUI_NM] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_GUI_NM_finished, dance_GUI_NM_reset),
  [ALT_NM] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_ALT_NM_finished, dance_ALT_NM_reset),
  [SFT_NM] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_SFT_NM_finished, dance_SFT_NM_reset),
+ [LANG_IN] = ACTION_TAP_DANCE_FN_ADVANCED (dance_LANG_IN_each, dance_LANG_IN_finished, dance_LANG_IN_reset)
 };
 
