41f481cce957217c1f48867ddbc820651433460e
Othi <mnpq.raven@gmail.com>
2018-12-31 12:43:33 +0100

    dedicated process_records for custom keycodes and specific keycode handling

--- users/othi/rules.mk
+++ users/othi/rules.mk
@@ -1,4 +1,5 @@
-SRC += othi.c
+SRC += othi.c \
+       process_records.c
 
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
 	SRC += tap_dance.c
@@ -8,24 +9,4 @@ ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
 	SRC += rgb_light.c
 endif
 EXTRAFLAGS += -flto			# Used to make code smaller
-
-# ENABLE
-UNICODE_ENABLE = yes	# Used for unicode character emulation
-EXTRAKEY_ENABLE = yes 	# OS signals like volume control
-TAP_DANCE_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-
-# DISABLE
-ifndef BLUETOOTH_ENABLE
-	BLUETOOTH_ENABLE = no 	# No bluetooth
-endif
-COMMAND_ENABLE = no			# Some bootmagic thing i dont use
-BOOTMAGIC_ENABLE = no 		# Access to EEPROM settings, not needed
-CONSOLE_ENABLE = no			# Allows console output with a command
-SLEEP_LED_ENABLE = no  		# Breathes LED's when computer is asleep. Untested.
-NKRO_ENABLE = no 			# Default is 6KRO which is plenty
-MIDI_ENABLE = no 			# Untested feature
-FAUXCLICKY_ENABLE = no 		# Emulates clicks using speaker
-KEY_LOCK_ENABLE = no 		# Allows locking any key. Not used
-API_SYSEX_ENABLE = no 		# Allows OS to send signals.
 
