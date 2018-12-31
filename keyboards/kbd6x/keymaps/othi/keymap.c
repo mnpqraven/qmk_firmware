46332b76cd90553cab92df35be5db53bcdbfb0bd
Othi <mnpq.raven@gmail.com>
2018-12-31 12:39:54 +0100

    update keymap with new BRCKS layer and tap dances

--- keyboards/kbd6x/keymaps/othi/keymap.c
+++ keyboards/kbd6x/keymaps/othi/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MT(MOD_LGUI,KC_TAB),    LT(NM_MODE,KC_Q),    KC_W,    LT(ACCENT,KC_F),    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_LBRC, KC_RBRC, KC_BSPC,
       TD(CTL_NM),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,           KC_QUOT, KC_ENT,
       TD(SFT_NM),             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    MT(MOD_LCTL,KC_COMM), MT(MOD_LSFT,KC_DOT),  MT(MOD_LALT,KC_SLSH),        LM(CL,MOD_LGUI|MOD_LSFT), TT(NM_MODE),
-      _______, TD(GUI_NM),                     TD(ALT_NM),              		KC_SPC,    LM(CL,MOD_LGUI|MOD_LALT), OSL(ACCENT)    , _______
+      _______, MO(BRCKS),                     TD(ALT_NM),            	KC_SPC,    LM(CL,MOD_LGUI|MOD_LALT), OSL(ACCENT)    , _______
       ),
   [NM_MODE] = LAYOUT(
       KC_GRV,           KC_MPRV,    KC_MNXT,    KC_MPLY,    KC_END,      _______,     _______,     _______,     _______,     _______,     KC_HOME,    _______,  _______,  RESET, KC_INS,
@@ -17,17 +17,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, TD(CTL_NM),                     TD(ALT_NM),                    KC_SPC,    LM(CL,MOD_LGUI|MOD_LALT), OSL(ACCENT), _______
       ),
   [FNUM] = LAYOUT(
-      _______,       KC_F1,  KC_F2, KC_F3,    KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,    KC_F11,  KC_F12,  _______, _______,
+      _______,          KC_F1,  KC_F2, KC_F3,    KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,    KC_F11,  KC_F12,  _______, _______,
       _______,          _______,    _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______, _______,
       _______,          _______,    _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,
-      _______, 	        _______,    _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,
+      _______,          _______,    _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,
       _______,             _______,         _______,            _______,         _______, _______,  _______
       ),
   [VI_MODE] = LAYOUT(
-      KC_GRV,         KC_MPRV,  KC_MNXT, KC_MPLY,    LSFT(KC_END),     _______,     _______,     _______,     _______,     _______,     LSFT(KC_HOME),    KC_F11,  KC_F12,  RESET, KC_INS,
+      KC_GRV,           KC_MPRV,  KC_MNXT, KC_MPLY,    LSFT(KC_END),     _______,     _______,     _______,     _______,     _______,     LSFT(KC_HOME),    KC_F11,  KC_F12,  RESET, KC_INS,
       LGUI(KC_TAB),     _______,    LSFT(LCTL(KC_RGHT)),  _______,    _______,    _______,    _______,    LSFT(KC_UP),    _______,    _______,    _______,     _______, TG(CL), KC_BSPC,
-      _______,            _______,    _______,    _______,      _______,    _______,    LSFT(LCTL(KC_LEFT)),   LSFT(KC_DOWN),   LSFT(KC_RGHT),     _______,    KC_SCLN,  KC_QUOT, KC_LGUI,
-      KC_LSFT,      	    _______,  _______, _______,      _______,    LSFT(LCTL(KC_LEFT)),    _______,    _______,    _______,    _______,    KC_SLSH,    OSM(MOD_LSFT), TO(CL),
+      _______,          _______,    _______,    _______,      _______,    _______,    LSFT(LCTL(KC_LEFT)),   LSFT(KC_DOWN),   LSFT(KC_RGHT),     _______,    KC_SCLN,  KC_QUOT, KC_LGUI,
+      KC_LSFT,          _______,  _______, _______,      _______,    LSFT(LCTL(KC_LEFT)),    _______,    _______,    _______,    _______,    KC_SLSH,    OSM(MOD_LSFT), TO(CL),
       _______, TD(CTL_NM),                     TD(ALT_NM),                 KC_SPC,    LM(CL,MOD_LGUI|MOD_LALT), OSL(ACCENT), _______
       ),
   [ACCENT] = LAYOUT(
@@ -49,5 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,      _______, _______,      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,
       _______,   _______,         _______,         _______,         _______,   _______,      _______
       ),
+  [BRCKS] = LAYOUT(
+      _______,   _______,  _______, _______,    _______,          _______,     _______,     _______,     _______,     _______,     _______,    _______,  _______,  _______, _______,
+      _______,      _______,    _______,  _______,    _______,    _______,    _______,    TD(QUOT),    KC_U,    _______,    _______,     _______, _______, _______,
+      _______,        _______,    _______,    _______,      _______,    _______,    _______,   TD(R_BRC),   TD(A_BRC),     TD(S_BRC),    KC_ENT,  _______,   _______,
+      _______,      _______, _______,      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,
+      _______,   _______,         _______,         KC_LSFT,         _______,   _______,      _______
+      ),
 };
-
