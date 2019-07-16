2d92165e072969497b279d9b706c4a7243c57640
Othi <mnpq.raven@gmail.com>
2019-07-16 15:28:44 +0200

    temp fix to colors TODO: find leds for kbd6x

--- keyboards/kbdfans/kbd6x/keymaps/othi/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/othi/keymap.c
@@ -19,7 +19,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CL] = LAYOUT(
       F_ESC, 					 KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,        KC_EQL,     KC_BSLS,    KC_DEL,
-      KC_TAB,       LT(NM_MODE,KC_Q),    KC_W,    LT(ACCENT,KC_F),    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_LBRC, KC_RBRC, KC_BSPC,
+      KC_TAB,       LT(NM_MODE,KC_Q),    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_LBRC, KC_RBRC, KC_BSPC,
       MT(MOD_LCTL,KC_BSPC),      KC_A,    KC_R,    KC_S,    LT(BRCKS, KC_T),    KC_D,    KC_H,    LT(BRCKS, KC_N),    KC_E,    KC_I,    KC_O,           KC_QUOT, KC_ENT,
       TD(SFT_NM),                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    MT(MOD_LCTL,KC_COMM), MT(MOD_LSFT,KC_DOT),  MT(MOD_LALT,KC_SLSH),        LM(CL,MOD_LGUI|MOD_LSFT), TT(NM_MODE),
       _______, KC_LGUI,                     TD(ALT_NM),              		KC_SPC,    LM(CL,MOD_LGUI|MOD_LALT), OSL(ACCENT)    , _______
@@ -42,7 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [ACCENT] = LAYOUT(
       _______,       KC_F1,  KC_F2, KC_F3,    KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,  KC_F12,  _______, _______,
       _______,         RGB_TOG,    RGB_MODE_PLAIN,  RGB_LYR,    RGB_MOD,    _______,    _______,    _______,    DE_UE,    _______,    _______,     _______, _______, _______,
-      _______,            DE_AE,    UC_Z,    DE_SS,      _______,    _______,    _______,   _______,   _______,     _______,    DE_OE,  _______, _______,
+      _______,            DE_AE,    _______,    DE_SS,      _______,    _______,    _______,   _______,   _______,     _______,    DE_OE,  _______, _______,
+      _______,     _______,  _______, _______,      _______,    _______,    _______,    _______,    _______,    _______,        _______, _______, TO(CL),
+      _______,             EPRM,         _______,            _______,         _______, _______,  _______
+      ),
+  [BRCKS] = LAYOUT(
+      _______,       _______,  _______, _______,    _______,     _______,     _______,     _______,     _______,     _______,     _______,    _______,  _______,  _______, _______,
+      _______,      _______,    _______,  _______,    _______,    _______,    _______,    TD(QUOT),    TD(GRV),    _______,    _______,     _______, _______, _______,
+      _______,            _______,    KC_SLSH,    KC_COMM,      KC_DOT,    _______,    _______,   TD(R_BRC),   TD(A_BRC),     TD(S_BRC),    KC_ENT,  _______, _______,
       _______,     _______,  _______, _______,      _______,    _______,    _______,    _______,    _______,    _______,        _______, _______, TO(CL),
       _______,             EPRM,         _______,            _______,         _______, _______,  _______
       ),
