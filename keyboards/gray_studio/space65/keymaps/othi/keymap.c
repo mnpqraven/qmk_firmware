24d09617d040e8b2ce17c59055e3f2bc101ebf5d
Othi <mnpq.raven@gmail.com>
2020-08-25 13:30:58 +0200

    scape65 revamp

--- keyboards/gray_studio/space65/keymaps/othi/keymap.c
+++ keyboards/gray_studio/space65/keymaps/othi/keymap.c
@@ -18,25 +18,33 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [CL] = LAYOUT( \
-        KC_ESC,  KC_1,              KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_BSPC,   KC_DEL,
-        KC_TAB,  LT(NM_MODE,KC_Q),  RGB_LYR,   RGB_TOG,   TEST,   KC_G,   KC_J,   KC_L,   KC_U,   KC_Y,    KC_SCLN,    KC_LBRC, KC_RBRC, KC_BSLS,              KC_INS,
-        KC_CAPS, OSL(VI_MODE),              OSL(NM_MODE),   OSL(ACCENT),   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,    KC_O, KC_QUOT, KC_ENT,                        KC_PGUP,
-        TD(SFT_NM),  _______,       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,                KC_PGDN,
-        KC_LCTL, KC_LGUI,           KC_LALT,                KC_SPC, KC_SPC, MO(NM_MODE),         OSL(VI_MODE), OSL(NM_MODE),   KC_LEFT, KC_DOWN,                  RESET
-    ),
+    F_ESC,                KC_1,             KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_BSPC,   KC_DEL,
+    KC_TAB,               LT(NM_MODE,KC_Q), KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,              KC_PGUP,
+    KC_LCTL,              KC_A,             KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,                        KC_PGDN,
+    TD(SFT_NM),  _______, KC_Z,             KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, LM(CL,MOD_LGUI|MOD_LSFT), KC_UP,        MO(NM_MODE),
+    KC_LCTL, KC_LGUI,     KC_LALT,                KC_SPC, KC_SPC, MO(NM_MODE),         LM(CL,MOD_LGUI|MOD_LALT), LGUI(KC_SPC),   KC_LEFT, KC_DOWN,      RESET
+  ),
+[GAME] = LAYOUT( \
+    F_ESC,                KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_BSPC,   KC_DEL,
+    KC_TAB,               KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,              KC_PGUP,
+    KC_LCTL,              KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,                        KC_PGDN,
+    TD(SFT_NM),  _______, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, LM(CL,MOD_LGUI|MOD_LSFT), KC_UP,        MO(NM_MODE),
+    KC_LCTL, KC_LGUI,     KC_LALT,       KC_SPC, KC_SPC, MO(NM_MODE),         LM(CL,MOD_LGUI|MOD_LALT), TO(CL),   KC_LEFT, KC_DOWN,      KC_TRNS
+  ),
 [NM_MODE] = LAYOUT( \
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_MUTE, \
-        KC_TRNS, KC_Q, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_TRNS,          \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU,                   \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_VOLD,          \
-        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, TO(CL), KC_HOME, KC_PGDN, KC_END                     \
-    ),
-[VI_MODE] = LAYOUT( \
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_MUTE, \
-        KC_TRNS, KC_Q, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_TRNS,          \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU,                   \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_VOLD,          \
-        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, TO(CL), KC_HOME, KC_PGDN, KC_END                     \
-    ),
+    KC_TRNS, KC_MPRV, KC_MNXT,        KC_MPLY, KC_END,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, LCTL(KC_RIGHT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   DE_UE,   KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_TRNS,        KC_RIGHT,KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_ENT,  KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+[ACCENT] = LAYOUT( \
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_UE,   KC_TRNS, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,
+    KC_TRNS, DE_UE,   KC_TRNS, DE_SS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OE,   KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, UC_M_LN, UC_M_WI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_PGUP, KC_VOLD,
+    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  )
 };
 
